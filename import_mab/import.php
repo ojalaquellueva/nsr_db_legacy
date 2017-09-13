6fbeb71bc65905a523e080baa54af34e95d6e421
bboyle <bboyle@email.arizona.edu>
2017-09-12 17:34:01 -0700

    import_mab complete

--- import_mab/import.php
+++ import_mab/import.php
@@ -14,8 +14,6 @@ include "create_raw_data_tables.inc";
 // import raw data file
 include "import.inc";
 
-exit("\r\nExiting...");
-
 //////////////////////////////////////////
 // Standardize and normalize 
 //////////////////////////////////////////
@@ -28,32 +26,36 @@ include "alter_tbl_raw.inc";
 // Make fixes specific to this source, if any
 include "source_specific_fixes.inc";
 
+/* Not needed for this source
+
 // standardize hybrid x to plain ascii
 include "fix_hybrid_x.inc";
 
 // separate taxon name from author and dump to taxon field
-include "detect_rank.inc";
+include "detect_rank.inc";	
 
 // Normalize the raw data
 include "normalize_regions.inc";
 
 // Normalize the raw data
 include "normalize_occurrences.inc";
+*/	
 
 // standardize status codes
 include "standardize_status.inc";
 
-// Insert unique values into temp table
+// Insert unique values into temp table & rename to main table
 include "unique_occurrences.inc";
 
+/* Not needed for this source
 // Insert the occurrences
 include "insert_country_occurrences.inc";
-
-
-
 include "insert_explicit_absent_introduced.inc";
-include "insert_implied_absences.inc";
+*/
 
+/* Skip this! Algorithm handles endemics automatically
+include "insert_implied_absences.inc";
+*/
 //////////////////////////////////////////
 // Load to staging tables 
 //////////////////////////////////////////
@@ -64,9 +66,12 @@ include "load_distribution_staging.inc";
 
 // load metadata on regions covered by this source
 include "create_poldiv_source_staging.inc";
+
 include "load_poldiv_source_staging.inc";
 
+//exit("\r\nExiting...");
 // delete temporary tables, if any
+
 if ($drop_raw) {
 	include "cleanup.inc";
 }

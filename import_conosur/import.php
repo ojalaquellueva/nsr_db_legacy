248169fe0f008f99881bfc75f2f1492076c4f69a
bboyle <bboyle@email.arizona.edu>
2017-12-10 17:50:52 -0800

    Import templates for conosur & flbr + misc renames
    1. Added directories import_conosur and import_flbr,
    plus template files.
    2. Standardized naming convention for all raw data
    tables to use suffix "raw" rather than current mix
    of suffix and prefix.

---
+++ import_conosur/import.php
@@ -0,0 +1,44 @@
+<?php
+// Imports species distribution data from source USDA Plants
+
+include "params.inc";	// everything you need to set is here and in global_params.inc
+
+////////////// Import raw data file //////////////////////
+
+// create empty import table
+// These must be identical in structure to raw data file
+include "create_raw_data_tables.inc";
+
+// import raw data file
+include "import.inc";
+
+echo "Standardizing $tbl_raw:\r\n";
+
+// Add any extra columns and indexes needed
+include "alter_raw.inc";
+
+// Make fixes specific to this source, if any
+include "source_specific_fixes.inc";
+
+// standardize hybrid x to plain ascii
+include "fix_hybrid_x.inc";
+
+// separate taxon name from author and dump to taxon field
+include "detect_rank.inc";
+
+// load data from combined raw data table to standardized staging table
+include "create_distribution_staging.inc";
+include "load_staging.inc";
+
+// load metadata on regions covered by this source
+include "create_poldiv_source_staging.inc";
+include "load_poldiv_source_staging.inc"; // NOT READY
+
+// delete temporary tables, if any
+if ($drop_raw || $drop_raw_force) {
+	include "cleanup.inc";
+}
+
+//////////////////////////////////////////////////////////
+
+?>
